1613ab5901f7f50076c6b7c5ee34680c18daf201
DianeW0223 <dwatts@motor.com>
2017-06-19 11:15:06 -0400

    Reverse Number
    Enter a number to reverse and output the number in reverse order.

---
+++ Homwork1/Homwork1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homwork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Diane Watts
+             * Homework 1
+             * 06/14/2017
+             */
+            Console.WriteLine("Enter a Number");
+            int InputNumber = Convert.ToInt32(Console.ReadLine());
+            //Converting number to integer
+            int Reverse = 0;
+            while (InputNumber > 0)
+            {
+                //Reversing the numbers through math until the number is no longer greater than zero
+                int Remainder = InputNumber % 10; 
+                Reverse = Remainder + (Reverse * 10); 
+                InputNumber = InputNumber / 10; 
+
+            }
+            Console.WriteLine("Reverse Number = {0}", Reverse);
+            //End of Code
+        }
+    }
+}
